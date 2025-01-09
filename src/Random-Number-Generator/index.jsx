aed281787304e6a6d628de88e5ab83336c26af2b
JzwylFSE <cjsimeon090@gmail.com>
2025-01-10 00:29:00 +0100

    updated random number generator and  styling

--- src/Random-Number-Generator/index.jsx
+++ src/Random-Number-Generator/index.jsx
@@ -3,15 +3,31 @@ import styles from "./styles.module.css";
 import { Link } from "react-router-dom";
 
 export default function RandomNumberGenerator() {
-  const [randomNumbers, setRandomNumbers] = useState([0, 0, 0]);
+  const [randomNumbers, setRandomNumbers] = useState([0, 0, 0, 0, 0]);
+  const [guessedNumbers, setGuessedNumbers] = useState([]);
   const min = 1;
-  const max = 100;
+  const max = 250;
+
+  const targetNumbers = [3, 9, 27, 81, 243];
 
   const rollNumbers = () => {
-    const newNumbers = Array.from({ length: 3 }, () =>
-      Math.floor(Math.random() * max) + min
-    );
+    if (guessedNumbers.length === targetNumbers.length) 
+      return;
+    const numbersSet = new Set();
+
+    while (numbersSet.size < 5) {
+      const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      numbersSet.add(randomNumber);
+    }
+
+    const newNumbers = Array.from(numbersSet);
     setRandomNumbers(newNumbers);
+
+    const newGuesses = targetNumbers.filter(
+      (num) => newNumbers.includes(num) && !guessedNumbers.includes(num)
+    );
+
+    setGuessedNumbers([...guessedNumbers, ...newGuesses]);
   };
 
   return (
@@ -19,18 +35,35 @@ export default function RandomNumberGenerator() {
       <Link to="/" className={styles.homeLink}>
         Home
       </Link>
-      <div className={styles.content}>
-        <h2>Get 3 random numbers from 0 to 100</h2>
-        <button onClick={rollNumbers} className={styles.myBtn}>
-          Roll Numbers
-        </button>
-        <div className={styles.results}>
-          {randomNumbers.map((num, index) => (
-            <div key={index} className={styles.resultBox}>
-              {num}
-            </div>
-          ))}
-        </div>
+      <h2>Guess correctly 5 random numbers between 1 and 250</h2>
+      <div className={styles.results}>
+        {randomNumbers.map((num, index) => (
+          <div
+            key={index}
+            className={`${styles.resultBox} ${
+              guessedNumbers.includes(num) ? styles.guessed : ""
+            }`}
+          >
+            {num}
+          </div>
+        ))}
+      </div>
+      <button
+        onClick={rollNumbers}
+        disabled={guessedNumbers.length === targetNumbers.length}
+        className={styles.rollButton}
+      >
+        Roll Numbers
+      </button>
+      <div className={styles.status}>
+        {guessedNumbers.length === targetNumbers.length ? (
+          <p className={styles.statussuccess}>You did it!🎉 You guessed all the numbers!</p>
+        ) : (
+          <p>
+            Guessed Numbers: {guessedNumbers.join(", ")} (
+            {targetNumbers.length - guessedNumbers.length} left)
+          </p>
+        )}
       </div>
     </div>
   );
